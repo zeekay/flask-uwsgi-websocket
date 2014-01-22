cf18e9d272ef3ed6afefe0e9d6ef9f9212f0b138
zeekay <zk@monoid.io>
2014-01-21 20:12:49 -0800

    v0.1.0

--- flask_uwsgi_websocket/__init__.py
+++ flask_uwsgi_websocket/__init__.py
@@ -5,7 +5,7 @@ High-performance WebSockets for your Flask apps powered by `uWSGI <http://uwsgi-
 '''
 
 __docformat__ = 'restructuredtext'
-__version__ = '0.0.3'
+__version__ = '0.1.0'
 __license__ = 'MIT'
 __author__  = 'Zach Kelling'
 
