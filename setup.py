6d9de91b5f9859c0610698883a4c630484f31f92
zeekay <zk@monoid.io>
2014-01-22 06:35:58 -0800

    v0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Flask-uWSGI-WebSocket',
-    version='0.2.1',
+    version='0.2.2',
     url='https://github.com/zeekay/flask-uwsgi-websocket',
     license='MIT',
     author='Zach Kelling',
