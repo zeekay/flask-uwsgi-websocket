e7a228aabd5a7fbe4f5e6ca66f74642ed4aac115
Zach Kelling <zk@monoid.io>
2014-04-23 12:43:00 -0500

    v0.2.5

--- flask_uwsgi_websocket/__init__.py
+++ flask_uwsgi_websocket/__init__.py
@@ -5,7 +5,7 @@ High-performance WebSockets for your Flask apps powered by `uWSGI <http://uwsgi-
 '''
 
 __docformat__ = 'restructuredtext'
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __license__ = 'MIT'
 __author__  = 'Zach Kelling'
 
